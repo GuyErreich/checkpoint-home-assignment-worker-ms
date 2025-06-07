0971da1d61a271dc903257c1dc2947218b867e6e
GuyErreich <gerreich.dev@gmail.com>
2025-06-07 11:17:50 +0300

    ruff format fix

--- src/worker/main.py
+++ src/worker/main.py
@@ -7,27 +7,25 @@ from mypy_boto3_sqs import SQSClient
 from mypy_boto3_s3 import S3Client
 from mypy_boto3_sqs.type_defs import MessageTypeDef
 
-sqs: SQSClient = boto3.client('sqs')
-s3: S3Client = boto3.client('s3')
+sqs: SQSClient = boto3.client("sqs")
+s3: S3Client = boto3.client("s3")
 
-SQS_URL: str = os.environ['SQS_QUEUE_URL']
-S3_BUCKET: str = os.environ['S3_BUCKET']
+SQS_URL: str = os.environ["SQS_QUEUE_URL"]
+S3_BUCKET: str = os.environ["S3_BUCKET"]
 
 
 def poll_messages() -> None:
     try:
         messages: list[MessageTypeDef] = sqs.receive_message(
-            QueueUrl=SQS_URL,
-            MaxNumberOfMessages=5,
-            WaitTimeSeconds=10
-        ).get('Messages', [])
+            QueueUrl=SQS_URL, MaxNumberOfMessages=5, WaitTimeSeconds=10
+        ).get("Messages", [])
 
         for msg in messages:
-            body: str = msg['Body']
+            body: str = msg["Body"]
             key: str = f"message-{int(time.time())}.json"
             s3.put_object(Bucket=S3_BUCKET, Key=key, Body=body)
 
-            sqs.delete_message(QueueUrl=SQS_URL, ReceiptHandle=msg['ReceiptHandle'])
+            sqs.delete_message(QueueUrl=SQS_URL, ReceiptHandle=msg["ReceiptHandle"])
             print(f"Processed message to {key}")
 
     except ClientError as e:
